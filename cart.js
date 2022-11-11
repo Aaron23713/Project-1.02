e0c22cd85f9b71947598d7ce050c4d2c75d8ce1a
Aaron23713 <nyaangao@dukes.jmu.edu>
2022-11-11 01:26:26 -0500

    Newest

--- cart.js
+++ cart.js
@@ -1,22 +1,70 @@
 {
-    var currentCart = new Array();
-}
-function updateCart() {
-    var image = document.getElementById('hello');
-    document.getElementsByName
-   currentCart.push(image);
-   for(let i = 0; i < currentCart.length; i++){
-    console.log(currentCart[i]);
-    console.log(currentCart.length);
-}
+    var currentCartItem = new Array();
+    var currentCartPrice = new Array();
     
+}
+function updateCartItemBlueCheeseBurger() {
+    var image = document.getElementById('Blue Cheese Burger');
+    document.getElementsByName
+   currentCartItem.push(image);
+   for(let i = 0; i < currentCartItem.length; i++){
+    console.log(currentCartItem[i]);
+    console.log(currentCartItem.length);
+}
+}
+function updateCartItemDoubleHamburger() {
+    var image = document.getElementById('Double Hamburger');
+    document.getElementsByName
+   currentCartItem.push(image);
+   for(let i = 0; i < currentCartItem.length; i++){
+    console.log(currentCartItem[i]);
+    console.log(currentCartItem.length);
+}
+}
+function updateCartPrice() {
+    var image = document.getElementById('image');
+    document.getElementsByName
+   currentCartPrice.push(image);
+   for(let i = 0; i < currentCartPrice.length; i++){
+    console.log(currentCartPrice[i]);
+    console.log(currentCartPrice.length);
+    
+}
+}
+function updateCartPrice() {
+    var image = document.getElementById('ExpensivePrice');
+    document.getElementsByName
+   currentCartPrice.push(image);
+   for(let i = 0; i < currentCartPrice.length; i++){
+    console.log(currentCartPrice[i]);
+    console.log(currentCartPrice.length);
+    
+}
+}
+function updateCartItemDessert() {
+    var image = met.getElementById('nameDessert');
+    document.getElementsByName
+    sessionStorage.setItem(1,image);
+   currentCartItem.push(image);
+   for(let i = 0; i < currentCartItem.length; i++){
+    console.log(currentCartItem[i]);
+    console.log(currentCartItem.length);
+}
+}
+function updateCartPriceDessert() {
+    var image = document.getElementById('priceDessert');
+    document.getElementsByName
+   currentCartPrice.push(image);
+   for(let i = 0; i < currentCartPrice.length; i++){
+    console.log(currentCartPrice[i]);
+    console.log(currentCartPrice.length);
+    
+}
 }
 function Storage(){
-    const currentCart = new Array();
-    currentCart.push(updateCart());
+    for(let i = 0; i < currentCartPrice.length; i++){
+    console.log(currentCartPrice[i]);
     
-    for(let i = 0; i < currentCart.length; i++){
-    console.log(currentCart[i]);
 }
     
 }
